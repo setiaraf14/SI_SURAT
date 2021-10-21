d731f83895752dd0ed4f808579b79ce0bc6cea08
setiaraf14 <setia.rafif14@gmail.com>
2021-10-21 22:42:31 +0700

    first commit

---
+++ resources/views/backend/layout-backend/content.blade.php
@@ -0,0 +1,75 @@
+@extends('backend.layout-backend.main')
+
+@section('bootstrap')
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+@endsection
+
+@section('permohonan-kk', 'active')
+@section('title', '| Permohonan KK')
+
+@section('judul')
+    <h1>Tabel Data </h1>
+@endsection
+
+@section('ckeditor')
+<script src="https://cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header d-flex justify-content-between">
+                        <h3 class="card-title">Data </h3>
+                    </div>
+                    <!-- /.card-header -->
+
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="tabel-data" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Nomor</th>
+                                        <th>Nama</th>
+                                        <th>Tempat/Tangal Lahir</th>
+                                        <th>Jenis Kelamin</th>
+                                        <th>Status</th>
+                                        <th>No KTP</th>
+                                        <th>Kewarganegaraan</th>
+                                        <th>Agama</th>
+                                        <th>Pekerjaan</th>
+                                        <th>Foto KTP Suami</th>
+                                        <th>Foto KTP Istri</th>
+                                        <th>Alamat</th>
+                                        <th>Approve</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                        
+                    </div>
+                </div>   
+            </div>
+        </div>
+    </div>
+    @section('js-bootstrap')
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    @endsection
+    @section('data-table')
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+        <script>
+            $(document).ready(function(){
+                $('#tabel-data').DataTable();
+            });
+        </script>
+    @endsection 
+        <script>
+            CKEDITOR.replace( 'summary_ekskul' );
+        </script>
+@endsection
---
+++ resources/views/backend/layout-backend/side-menu.blade.php
@@ -0,0 +1,106 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <span class="brand-text font-weight-light">Template Admin</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="{{ asset('admin/dist/img/AdminLTELogo.jpg') }}" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block"></a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link @yield('dashboard') ">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link @yield('permohonan-ktp') ">
+              <i class="nav-icon fas fa-id-card"></i>
+              <p>
+                Permohonan KTP
+              </p>
+            </a>
+          </li>
+          <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link @yield('permohonan-kk') ">
+              <i class="nav-icon fas fa-file-alt"></i>
+              <p>
+                Permohonan KK
+              </p>
+            </a>
+          </li>
+        <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link @yield('berita') ">
+            <i class="fas fa-newspaper"></i>
+            <p>
+                Berita Kecamatan
+            </p>
+            </a>
+        </li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link"  @yield('user-role')>
+              <i class="nav-icon fas fa-chart-pie"></i>
+              <p>
+                User & Role
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>RT</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>RW</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>User</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link @yield('rating') ">
+              <i class="nav-icon fas fa-star-half-alt"></i>
+              <p>
+                Rating
+              </p>
+            </a>
+          </li>
+          <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link">
+              <i class="far fa-hand-point-left"></i>
+              <p>
+                Back To Homepage
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
