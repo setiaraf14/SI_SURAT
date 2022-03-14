7731bd2cff33e39e057d5d6594a6bdc29789f6ee
Rafif Setia Nugraha <rafif@importir.co>
2022-03-14 08:26:05 +0700

    update branch

--- resources/views/index-surat/index.blade.php
+++ resources/views/index-surat/index.blade.php
@@ -22,10 +22,11 @@
                 <div class="card">
                     <div class="card-header d-flex justify-content-between">
                         <h3 class="card-title">Data Index Surat</h3>
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                            Input Index Surat
-                        </button>
-
+                        @if(Auth::user()->role == "pegawai")
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                Input Index Surat
+                            </button>
+                        @endif
                         {{-- modal-input-surat-masuk --}}
                         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog" role="document">
